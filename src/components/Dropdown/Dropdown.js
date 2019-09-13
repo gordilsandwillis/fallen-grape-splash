f96bf90fe441b15fe5985973ff3337644b884c0e
dillon <dpett122@gmail.com>
2019-09-13 18:20:30 -0400

    custom dropdown indicator

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { colors, animations } from 'src/styles'
 import Select from 'react-select'
-
+import { colors, animations } from 'src/styles'
+import DropdownArrowSvg from 'src/assets/images/dropdown_arrow.svg'
 // const TextAlign = styled.div`
 //   text-align: ${ ({ align }) => align };
 // `
@@ -60,16 +60,27 @@ const SelectStyled = styled(Select)`
   div:hover {
     color: ${ colors.grey }
   }
+	span {
+		background-color: transparent;
+	}
 `
 
+const DropdownArrowSvgStyled = styled(DropdownArrowSvg)`
+	height:16px;
+	width: 16px;
+`
+const DropdownIndicator = () => <div><DropdownArrowSvgStyled/></div>
 class Dropdown extends React.Component {
 	componentWillReceiveProps (props) {
 		if (props.clearValue) this.setState({ value: null })
 	}
+
 	render () {
 		const { title, isMulti, items, onChange, value } = this.props
 		return <div>
 			<SelectStyled
+				components={{ DropdownIndicator }}
+				arrowrenderer={this.arrowRenderer}
 				isMulti={isMulti}
 				value={value}
 				isClearable
