e0cbbfcc4d968c0ef2ea2a05754ad0fbf496eaa9
dillon <dpett122@gmail.com>
2019-09-18 11:05:11 -0400

    (fix) iOS Safari Animated ATF height

--- src/components/ATF/ATF.js
+++ src/components/ATF/ATF.js
@@ -95,7 +95,7 @@ const AnimatedGradient = styled.div`
 		margin: 0;
 		position: absolute;
 		width: 100%;
-		height: 100vh;
+		height: 100%;
 		font-family: "Exo", sans-serif;
 		color: #fff;
 		background: radial-gradient(rgba(25,150,154, .9), rgba(110,178,177, .1), rgba(52,207,214, .75), rgba(166,172,160, .1));
