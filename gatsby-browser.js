9b1f84ade82b5a9b371c1876ed5101f88607c775
dillon <dpett122@gmail.com>
2019-09-18 17:02:22 -0400

    fix scrolling on transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,22 @@
 
 import onRouteUpdate from './segment/onRouteUpdate'
 
-export { onRouteUpdate }
+const transitionDelay = 750
+
+const shouldUpdateScroll = ({
+	routerProps: { location },
+	getSavedScrollPosition,
+}) => {
+	if (location.action === 'PUSH') {
+		window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
+	} else {
+		const savedPosition = getSavedScrollPosition(location)
+		window.setTimeout(
+			() => window.scrollTo(...(savedPosition || [0, 0])),
+			transitionDelay
+		)
+	}
+	return false
+}
+
+export { onRouteUpdate, shouldUpdateScroll }
