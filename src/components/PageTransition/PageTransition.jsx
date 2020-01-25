1dac67f767cabeafbc9a427c02970fe7ef5ece59
Matt Gordils <mattgordils@gmail.com>
2020-01-24 19:57:13 -0500

    add styles for better page transitions

--- src/components/PageTransition/PageTransition.jsx
+++ src/components/PageTransition/PageTransition.jsx
@@ -3,21 +3,39 @@ import {
 	TransitionGroup,
 	Transition as ReactTransition
 } from 'react-transition-group'
+
 const timeout = 750
+
 const getTransitionStyles = {
 	entering: {
+		position: `absolute`,
+		top: 0,
+		left: 0,
+		right: 0,
+		overflow: `hidden`,
 		position: `absolute`,
 		opacity: 0,
 	},
 	entered: {
+		position: `absolute`,
+		top: 0,
+		left: 0,
+		right: 0,
+		overflow: `hidden`,
 		transition: `opacity ${ timeout }ms ease-in-out`,
 		opacity: 1,
 	},
 	exiting: {
+		position: `absolute`,
+		top: 0,
+		left: 0,
+		right: 0,
+		overflow: `hidden`,
 		transition: `opacity ${ timeout }ms ease-in-out`,
 		opacity: 0,
 	},
 }
+
 class PageTransition extends React.PureComponent {
 	render () {
 		const { children, location } = this.props
