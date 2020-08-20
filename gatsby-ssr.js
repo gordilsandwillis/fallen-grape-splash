f13a817e5d17e72a51e66a925400ed10716a118c
Matt Gordils <mattgordils@gmail.com>
2020-08-20 16:09:28 -0400

    add ticktok tracking code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,3 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+
+import React from 'react'
+
+export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }) => {
+  if (process.env.NODE_ENV === `production`) {
+    return setHeadComponents([
+      <script
+				async
+				type="text/javascript"
+				key={`tiktok-tracking`}
+				src='https://analytics.tiktok.com/i18n/pixel/sdk.js?sdkid=BSUQI7PBAFSQRGNC8D00'
+			/>
+    ])
+  }
+}
