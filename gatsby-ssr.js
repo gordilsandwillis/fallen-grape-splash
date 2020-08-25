62308a620001a21e5914df498537b2d9c17a5c07
dillon <dpett122@gmail.com>
2020-08-25 12:39:16 -0400

    working on klaviyo fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,25 +7,23 @@
 import React from 'react'
 
 export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }) => {
-	const klaviyoCompanyId = process.env.GATSBY_KLAVIYO_COMPANY_ID
+  const klaviyoCompanyId = process.env.GATSBY_KLAVIYO_COMPANY_ID
 
   if (process.env.NODE_ENV === `production`) {
     setHeadComponents([
       <script
-				async
-				type="text/javascript"
-				key={`tiktok-tracking`}
-				src='https://analytics.tiktok.com/i18n/pixel/sdk.js?sdkid=BSUQI7PBAFSQRGNC8D00'
-			/>
-    ])
+        async
+        type="text/javascript"
+        key={`tiktok-tracking`}
+        src='https://analytics.tiktok.com/i18n/pixel/sdk.js?sdkid=BSUQI7PBAFSQRGNC8D00'
+      />,
+      <script
+        async
+        type="text/javascript"
+        key={`gatsby-plugin-klaviyo`}
+        src={`//static.klaviyo.com/onsite/js/klaviyo.js?company_id=${ klaviyoCompanyId }`}
+      />
 
-    setPostBodyComponents([
-			<script
-				async
-				type="text/javascript"
-				key={`gatsby-plugin-klaviyo`}
-				src={`//static.klaviyo.com/onsite/js/klaviyo.js?company_id=${ klaviyoCompanyId }`}
-			/>
-  	])
+    ])
   }
 }
--- src/components/KlaviyoSignup/KlaviyoSignup.jsx
+++ src/components/KlaviyoSignup/KlaviyoSignup.jsx
@@ -1,6 +1,7 @@
 import React, { useState, Component } from 'react'
 import styled from '@emotion/styled'
 import withSizes from 'react-sizes'
+import { subscribe } from 'klaviyo-subscribe'
 
 import { validateEmail } from 'src/utils/validations'
 
@@ -39,158 +40,171 @@ const SubmitButton = styled(Button)`
 	}
 `
 
-const CustomForm = ({ 
-	status,
-	onSubmit,
-	className,
-	placeholder,
-	label,
-	size,
-	buttonTheme,
-	id,
-	buttonText,
-	setSuccessState
+const CustomForm = ({
+  status,
+  onSubmit,
+  className,
+  placeholder,
+  label,
+  size,
+  buttonTheme,
+  id,
+  buttonText,
+  setSuccessState
 }) => {
+  const [email, setEmail] = useState('')
 
-	const [email, setEmail] = useState('')
+  const submit = () => {
+    email && validateEmail(email) && onSubmit(email)
+    // run setSuccessState() to put the page into the success state
+  }
 
-	const submit = () => {
-		email && validateEmail(email) && onSubmit(email)
-		// run setSuccessState() to put the page into the success state
-	}
+  const renderButtonText = buttonStatus => {
+    let text = 'Submit'
+    if (buttonStatus === 'sending') {
+      // text = <Loader />
+      text = 'Sending'
+    } else if (buttonStatus === 'success') {
+      text = 'Thank You'
+    } else if (buttonStatus === 'error') {
+      text = 'Oh No!'
+    } else {
+      text = 'Submit'
+    }
+    return text
+  }
 
-	const renderButtonText = buttonStatus => {
-		let text = 'Submit'
-		if (buttonStatus === 'sending') {
-			// text = <Loader />
-			text = 'Sending'
-		} else if (buttonStatus === 'success') {
-			text = 'Thank You'
-		} else if (buttonStatus === 'error') {
-			text = 'Oh No!'
-		} else {
-			text = 'Submit'
-		}
-		return text
-	}
+  return (
 
-	return (
-
-		<FormWrapper className={className}>
-			<Grid
-    		small='[1]'
-    		medium='[8] [4]'
-    		large='[11] [7] 6'
-    		larger='[8] [5] 11'
-    		rowGap='16px'
-    		colGap='16px'
-    	>
-    		<div>
-    			<SplashInput
-        		label={label || 'Enter email'}
-        		type="email"
-        		name="email"
-        		size={size}
-        		value={email}
-        		onChange={event => setEmail(event.target.value)}
-        	/>
-    		</div>
-    		<div>
-        	<SubmitButton
-        		// onClick={submit}
-        		onClick={setSuccessState}
-        		loading={status === 'sending'}
-        		disabled={status === 'sending' || !validateEmail(email)}
-        		setTheme={buttonTheme}
-        		iconPosition='right'
-        		icon='arrow_forward'
-        		size={size}
-        	>
-        		{buttonText || 'Let me know'}
-        	</SubmitButton>
-    		</div>
-    	</Grid>
-		</FormWrapper>
-	)
+    <FormWrapper className={className}>
+      <Grid
+        small='[1]'
+        medium='[8] [4]'
+        large='[11] [7] 6'
+        larger='[8] [5] 11'
+        rowGap='16px'
+        colGap='16px'
+      >
+        <div>
+          <SplashInput
+            label={label || 'Enter email'}
+            type="email"
+            name="email"
+            size={size}
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          <SubmitButton
+            onClick={submit}
+            loading={status === 'sending'}
+            disabled={status === 'sending' || !validateEmail(email)}
+            setTheme={buttonTheme}
+            iconPosition='right'
+            icon='arrow_forward'
+            size={size}
+            type='submit'
+          >
+            {buttonText || 'Let me know'}
+          </SubmitButton>
+        </div>
+      </Grid>
+    </FormWrapper>
+  )
 }
 
 class KlaviyoSignup extends Component {
-	state = {
-		status: null
-	}
+  state = {
+    status: null
+  }
 
-	subscribe = email => {
-		const { listId } = this.props
-		const data = {
-			$fields: '$source,$email,$consent_method,$consent_form_id,$consent_form_version',
-			email: email,
-			g: listId,
-			$consent_method: 'Klaviyo Form',
-			$source: 'Newsletter Signup',
-			$consent_form_id: 'Y2pnRT',
-			$consent_form_version: 1108660
-		}
+  handleSubscribe = email => {
+    const { listId } = this.props
+    console.log({ listId, email })
+    subscribe(listId, email
+      // { any optional traits }
+    ).then(response => {
+      console.log({ response })
+      if (response && response.success) {
+        this.setState({ status: 'success' })
+        this.props.setSuccessState()
+      } else {
+        this.setState({ status: 'error' })
+      }
+    })
 
-		const params = new URLSearchParams(data).toString()
-		const endpoint = `https://a.klaviyo.com/ajax/subscriptions/subscribe`
+    // const data = {
+    // 	$fields: '$source,$email,$consent_method,$consent_form_id,$consent_form_version',
+    // 	email: email,
+    // 	g: listId,
+    // 	$consent_method: 'Klaviyo Form',
+    // 	$source: 'Newsletter Signup',
+    // 	$consent_form_id: 'Y2pnRT',
+    // 	$consent_form_version: 1108660
+    // }
 
-		const options = {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			},
-			method: 'POST',
-			body: params
-		}
+    // const params = new URLSearchParams(data).toString()
+    // const endpoint = `https://a.klaviyo.com/ajax/subscriptions/subscribe`
 
-		fetch(endpoint, options).then(res => {
-			return res.json()
-		}).then(json => {
-			if (json.errors.length === 0) {
-				this.setState({ status: 'success' })
-			} else {
-				this.setState({ status: 'error' })
-			}
-		})
-	}
+    // const options = {
+    // 	headers: {
+    // 		'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    // 	},
+    // 	method: 'POST',
+    // 	body: params
+    // }
 
-	render () {
-		const {
-			buttonTheme,
-			className,
-			winWidth,
-			size,
-			label,
-			placeholder,
-			buttonText,
-			setSuccessState
-		} = this.props
+    // fetch(endpoint, options).then(res => {
+    // 	return res.json()
+    // }).then(json => {
+    // 	if (json.errors.length === 0) {
+    //     this.setState({ status: 'success' })
+    //     this.props.setSuccessState()
+    // 	} else {
+    // 		this.setState({ status: 'error' })
+    // 	}
+    // })
+  }
 
-		const { message, status } = this.state
+  render () {
+    const {
+      buttonTheme,
+      className,
+      winWidth,
+      size,
+      label,
+      placeholder,
+      buttonText,
+      setSuccessState
+    } = this.props
 
-		return (
-		<div>
-			<CustomForm
-				id="klaviyo-subscribe"
-				buttonTheme={buttonTheme || 'default'}
-				status={status}
-				message={message}
-				onSubmit={this.subscribe}
-				className={className}
-				placeholder={placeholder}
-				label={label}
-				buttonText={buttonText}
-				setSuccessState={setSuccessState}
-				size={size || 'small'}
-			/>
-			{/* <div class="klaviyo-form-Y2pnRT"></div> */}
-		</div>
+    const { message, status } = this.state
 
-		)
-	}
+    return (
+      <div>
+        <CustomForm
+          id="klaviyo-subscribe"
+          buttonTheme={buttonTheme || 'default'}
+          status={status}
+          message={message}
+          onSubmit={this.handleSubscribe}
+          className={className}
+          placeholder={placeholder}
+          label={label}
+          buttonText={buttonText}
+          setSuccessState={setSuccessState}
+          size={size || 'small'}
+        />
+        {/* <div class="klaviyo-form-Y2pnRT"></div> */}
+      </div>
+
+    )
+  }
 }
 
 const sizesToProps = ({ width, height }) => ({
-	winWidth: width
+  winWidth: width
 })
 
 export default KlaviyoSignup
